cdfcadf15e18ddc8d14558644464095c09d13615
eveil <erick@erickveil.net>
2013-11-26 14:24:55 -0800

    first commit
    We use the socket listener from the MSN example with minor changes.
    We deal with preventing the listener from freezing the GUI by running it in its
    own thread.
    
    Pressing the go button starts the listener in another thread.
    
    To stop the listener, we kill its thread: not the best way to do this.
    
    In the real application, the thread will keep running along side the
    program, and the start/stop mechanism will effect simply skipping processes by
    setting a flag.

---
+++ test6_socket_listener/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace test6_socket_listener
+{
+    public partial class Form1 : Form
+    {
+        public bool ison = false;
+        public Thread listen_thread;
+        public AsynchListener listener=new AsynchListener();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_stop_Click(object sender, EventArgs e)
+        {
+            if (!this.ison)
+            {
+                this.ison = true;
+                state_button.BackColor = Color.Lime;
+
+                // Run the listener on its own thread so we can still use the GUI
+                this.listen_thread = new Thread(this.listener.StartListening);
+                // This will allow the thread to die when the program does.
+                this.listen_thread.IsBackground = true;
+                this.listen_thread.Start();
+            }
+            else
+            {
+                this.ison = false;
+                state_button.BackColor = Color.Red;
+                this.listen_thread.Abort();
+            }
+        }
+    }
+}
